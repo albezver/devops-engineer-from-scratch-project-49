8bd7130bd9db0720d17f186e0e51ec385ba74cbd
Alex Bezverchuk <albezver@gmail.com>
2026-08-14 12:41:53 -0500

    Renamed Even-Odd game function

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,9 @@
 from brain_games import engine
-from brain_games.games import even_odd
+from brain_games.games import get_question_and_right_answer
 
 
 def main():
-    engine.run_game(even_odd)
+    engine.run_game(get_question_and_right_answer)
 
 
 if __name__ == "__main__":
